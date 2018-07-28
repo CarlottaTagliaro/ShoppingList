9e304b0eef0b812837ed89d2864126ffbe3782d4
caramellaio <albertobombardelli123@gmail.com>
2018-07-28 21:01:26 +0200

    add MailSender (it works)
    NB: require a sender email, password setup

--- src/main/java/job_scheduler/AdviceSender.java
+++ src/main/java/job_scheduler/AdviceSender.java
@@ -5,13 +5,66 @@
  */
 package job_scheduler;
 
+import it.webproject2018.db.CategoriaProdotti;
+import it.webproject2018.db.Prodotto;
+import it.webproject2018.db.Utente;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.MessagingException;
+import job_scheduler.MailSender;
 /**
  *
  * @author caramellaio
  */
 public class AdviceSender implements Runnable {
+    
+    private String mail;
+    private String password;
+    private boolean run = true;
     @Override
     public void run() {
-        System.out.println("porco dio!");
+        /* qui c'e' solo codice esempio che ho usato per testare, va.*/
+        String dest_mail;
+        if (this.run) {
+            System.out.println("porco dio!");
+            mail = "web2018unitn@gmail.com";
+            password = "shoppinglist18";
+            ArrayList<Prodotto> products = new ArrayList<>();
+            products.add(new Prodotto(Integer.SIZE, "rastrello", "rastrella", 
+                                      "a", "a", new CategoriaProdotti("cose", "", "", null) ));
+            this.sendMailTo(new Utente("pippo","aaa", dest_mail, "lalala", false), products);
+        }
+        this.run = false;
+    }
+    
+    void sendMailTo(Utente user, ArrayList<Prodotto> products) {
+        String email = user.getEmail();
+        String header = "SHOPPING LIST: prodotti in scadenza";
+        try {
+            MailSender.Send(mail, password, user.getEmail(), header,
+                    this.generateMessage(user, products));
+        } catch (MessagingException ex) {
+            System.err.println("Fail sending email");
+            Logger.getLogger(AdviceSender.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    String generateMessage(Utente user, ArrayList<Prodotto> products) {
+        StringBuilder messageBuilder = new StringBuilder();
+        messageBuilder.append(String.format("Ciao: %s, hai dei prodotti in scadenza:\n",
+                                            user.getSurname()));
+        for (Prodotto p : products) {
+            messageBuilder.append(String.format("PRODOTTO %s, CATEGORIA: %s.\n",
+                                                p.getNome(), p.getCategoria().getNome()));
+        }
+        
+        messageBuilder.append("Buona Giornata da SHOPPING LIST.");
+        return messageBuilder.toString();
+    }
+    
+    public void setMailSender(String mail, String password) {
+        this.mail = mail;
+        this.password = password;
     }
 }
---
+++ src/main/java/job_scheduler/MailSender.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package job_scheduler;
+
+/**
+ *
+ * @author caramellaio
+ */
+
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/* send emails using GMAIL (copy paste code) */
+public class MailSender {
+    
+    public static void Send(final String from, final String password, String to, String title, String message_text) throws AddressException, MessagingException {
+         // Recipient's email ID needs to be mentioned.
+      // Sender's email ID needs to be mentioned
+
+      // Assuming you are sending email from localhost      
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+      // Get the default Session object.
+
+      Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(from, password);
+			}
+		  });
+      try {
+         // Create a default MimeMessage object.
+         MimeMessage message = new MimeMessage(session);
+
+         // Set From: header field of the header.
+         message.setFrom(new InternetAddress(from));
+
+         // Set To: header field of the header.
+         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+         // Set Subject: header field
+         message.setSubject(title);
+
+         // Now set the actual message
+         message.setText(message_text);
+
+         // Send message
+         Transport.send(message);
+         System.out.println("Sent message successfully....");
+      } catch (MessagingException mex) {
+          System.out.println(mex.toString());
+      }
+    }
+}
