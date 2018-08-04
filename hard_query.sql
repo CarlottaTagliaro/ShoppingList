aede8082577a28de4be0f7ab32be55b7bc79dd33
Davide <davide.zanella10@gmail.com>
2018-08-04 12:30:27 +0200

    Finished query to predict user purchase

--- hard_query.sql
+++ hard_query.sql
@@ -1,15 +1,24 @@
 -- to get product the user need to buy 
 
-select medie.ID_lista, medie.ID_prodotto, 
-	(medie.giorni_media - DATEDIFF(NOW(), ultimo.Data_acquisto)) as giorni_manc,
-    (medie.quant_media + medie.quant_media - ultimo.Quantita) as quant_manc from
+select * from
 
-	(select ID_lista, ID_prodotto, AVG(Quantita) as quant_media, AVG(diff) as giorni_media from 
-		(select a.ID_lista, a.ID_prodotto, a.Quantita, MIN(DATEDIFF(a.Data_acquisto, b.Data_acquisto)) as diff from 
-			(SELECT * FROM Liste_Prodotti_Acquistati order by Data_acquisto desc limit 5) as a,
-			(SELECT * FROM Liste_Prodotti_Acquistati order by Data_acquisto desc limit 1, 5) as b
-			
-			where DATEDIFF(a.Data_acquisto, b.Data_acquisto) > 0 group by a.Data_acquisto) as tmp) as medie
-	join 
-		(SELECT * FROM Liste_Prodotti_Acquistati order by Data_acquisto desc limit 1) as ultimo
-	on medie.ID_lista = ultimo.ID_lista AND medie.ID_prodotto = ultimo.ID_prodotto;
+	(select ID_lista, ID_prodotto, 
+		(giorni_media - DATEDIFF(NOW(), ultimo)) as giorni_manc,
+		(quant_media + quant_media - 
+			(select Quantita from Liste_Prodotti_Acquistati where ID_lista = medie.ID_lista AND ID_prodotto = medie.ID_prodotto AND Data_acquisto = medie.ultimo)
+			) as quant_manc from
+
+		(select ID_lista, ID_prodotto, AVG(Quantita) as quant_media, AVG(diff) as giorni_media, ultimo from
+
+			(select a.ID_lista, a.ID_prodotto, a.Quantita, MIN(DATEDIFF(b.Data_acquisto, a.Data_acquisto)) as diff, MAX(b.Data_acquisto) as ultimo from 
+				(SELECT * FROM Liste_Prodotti_Acquistati) as a,
+				(SELECT * FROM Liste_Prodotti_Acquistati) as b
+				
+				where DATEDIFF(a.Data_acquisto, b.Data_acquisto) < 0 AND a.ID_lista = b.ID_lista AND a.ID_prodotto = b.ID_prodotto
+				
+				group by a.Data_acquisto, a.ID_lista, a.ID_prodotto) as tmp
+			group by ID_lista, ID_prodotto) as medie) as final
+	where quant_manc > 0 and (ID_lista, ID_prodotto) not in (select ID_lista, ID_prodotto from Liste_Prodotti);
+
+-- giorni_manc: tra quanti giorni deve fare l'acquisto (se negativo doveva farlo giorni fa)
+-- quant_manc: quantità che manca da comprare tra "giorni_manc" giorni
