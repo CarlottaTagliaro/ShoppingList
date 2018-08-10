0d54eb42b3ecb82b47bed78b78c8a544db104d7f
Davide <davide.zanella10@gmail.com>
2018-08-10 16:38:47 +0200

    Passed user object after login

--- src/main/java/it/webproject2018/servlets/LoginServlet.java
+++ src/main/java/it/webproject2018/servlets/LoginServlet.java
@@ -31,10 +31,11 @@ public class LoginServlet extends HttpServlet {
             String password = request.getParameter("password");
             Utente user = dbManager.getUserAuthentication(username, password);
             if (user == null || !user.getEmail().equals(username)) {
+                request.getSession().removeAttribute("User");
                 response.sendRedirect(request.getContextPath() + "/login.jsp"); // No logged-in user found, so redirect to login page.
             }
             else {
-                request.getSession().setAttribute("IsAdmin", user.getIsAdmin());
+                request.getSession().setAttribute("User", user);
                 response.sendRedirect(request.getContextPath() + "/home.jsp");
                 // Logged-in user found, so just continue request.
             }
