736b85691334f8c1043a712c60af027b979c8c54
Carlotta Tagliaro <carlotta.tagliaro@gmail.com>
2018-08-16 10:51:25 +0200

    formatted code

--- src/main/java/it/webproject2018/customtags/ProductCard.java
+++ src/main/java/it/webproject2018/customtags/ProductCard.java
@@ -27,26 +27,26 @@ public class ProductCard extends SimpleTagSupport {
     
     @Override
     public void doTag() throws JspException, IOException {
-        String html = String.format("<div class=\"row card\">\n" +
-"                <div class=\"col-xs-3\">\n" +
-"                    <img class=\"imageList img-responsive\" src=\"%s\"/>\n" +
-"                </div>\n" +
-"                <div class=\"col-xs-6\">\n" +
-"                    <h4> <b> %s </b></h4>\n" +
-"                    <h6> %s </h6>\n" +
-"                    <p> %s </p>\n" +
-"                </div>\n" +
-"\n" +
-"                <div class=\"col-xs-3 myColumn\">\n" +
-"                    <div>\n" +
-"                        <label> Add to list: </label>\n" +
-"                        <button class=\"myButton\" text=\"+\">+</button>\n" +
-"                    </div>\n" +
-"                    <div>\n" +
-"                        <input type=\"checkbox\" value=\"true\" disabled=\"true\"> Already in a list\n" +
-"                    </div>\n" +
-"                </div>\n" +
-"            </div>", immagine, nome, categoria, descrizione);
+        String html = String.format("<div class=\"row card\">\n"
+                + "                <div class=\"col-xs-3\">\n"
+                + "                    <img class=\"imageList img-responsive\" src=\"%s\"/>\n"
+                + "                </div>\n"
+                + "                <div class=\"col-xs-6\">\n"
+                + "                    <h4> <b> %s </b></h4>\n"
+                + "                    <h6> %s </h6>\n"
+                + "                    <p> %s </p>\n"
+                + "                </div>\n"
+                + "\n"
+                + "                <div class=\"col-xs-3 myColumn\">\n"
+                + "                    <div>\n"
+                + "                        <label> Add to list: </label>\n"
+                + "                        <button class=\"myButton\" text=\"+\">+</button>\n"
+                + "                    </div>\n"
+                + "                    <div>\n"
+                + "                        <input type=\"checkbox\" value=\"true\" disabled=\"true\"> Already in a list\n"
+                + "                    </div>\n"
+                + "                </div>\n"
+                + "            </div>", immagine, nome, categoria, descrizione);
         getJspContext().getOut().write(html);
     }
 
