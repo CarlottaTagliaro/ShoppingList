fa8cda3fc7c1652a5088052b3686d8f83062a7fb
Stefano Sega <stefano.sega-1@studenti.unitn.it>
2018-10-11 17:46:01 +0200

    Implemented ChangeUserDetails and added two servlet to web.xml

---
+++ src/main/java/it/webproject2018/servlets/ChangeUserDetailsServlet.java
@@ -0,0 +1,60 @@
+package it.webproject2018.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.SecureRandom;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import it.webproject2018.db.daos.jdbc.JDBCUtenteDAO;
+import it.webproject2018.db.entities.Utente;
+import it.webproject2018.db.exceptions.DAOException;
+
+/**
+ *
+ * @author Stefano
+ */
+public class ChangeUserDetailsServlet extends HttpServlet {
+
+    private JDBCUtenteDAO JDBCUtente;
+
+    @Override
+    public void init() throws ServletException {
+        JDBCUtente = new JDBCUtenteDAO(super.getServletContext());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter w = response.getWriter();
+        try {
+            Utente user = (Utente) request.getSession().getAttribute("User");
+            String name = request.getParameter("name");
+            String surname = request.getParameter("surname");
+
+            user.setName((name==null) ? "" : name);
+            user.setSurname((surname==null) ? "" : surname);
+            
+            user = JDBCUtente.update(user);
+            Boolean ok = user != null;
+            
+            response.sendRedirect(request.getContextPath() + (!ok ? "/profile" : "/profile"));
+        } catch (DAOException e) {
+            w.println(e.getMessage());
+        }
+    }
+
+    static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    static SecureRandom rnd = new SecureRandom();
+
+    String randomString(int len) {
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
+        }
+        return sb.toString();
+    }
+}
