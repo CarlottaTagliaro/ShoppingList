eb0ec653b0a0a23800cdbef25643090eccf5e243
caramellaio <albertobombardelli123@gmail.com>
2018-10-28 18:34:53 +0100

    change query into stored procedure that creates notifications

--- hard_query.sql
+++ hard_query.sql
@@ -2,7 +2,12 @@
 
 -- se non va:
 -- set sql_mode= (SELECT replace(@@sql_mode, 'ONLY_FULL_GROUP_BY',''));
-select * from
+drop procedure if exists add_notifications;
+DELIMITER $$
+create procedure add_notifications()
+begin
+insert into Notifiche (ID_list, ID_prodotto, Giorni_mancanti, Quantita_mancanti, Creazione, Mail)
+select final.ID_lista, final.ID_prodotto, final.giorni_manc, final.quant_manc, now() as ora, false as Mail from
 
 	(select ID_lista, ID_prodotto, 
 		(giorni_media - DATEDIFF(NOW(), ultimo)) as giorni_manc,
@@ -21,6 +26,7 @@ select * from
 				group by a.Data_acquisto, a.ID_lista, a.ID_prodotto) as tmp
 			group by ID_lista, ID_prodotto) as medie) as final
 	where quant_manc > 0 and (ID_lista, ID_prodotto) not in (select ID_lista, ID_prodotto from Liste_Prodotti);
-
+end $$
+delimiter ;
 -- giorni_manc: tra quanti giorni deve fare l'acquisto (se negativo doveva farlo giorni fa)
 -- quant_manc: quantità che manca da comprare tra "giorni_manc" giorni
