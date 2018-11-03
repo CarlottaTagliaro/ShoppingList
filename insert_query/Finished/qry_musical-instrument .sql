a41b332f8b1d593103b7bd6bb2e0464a62beb4a9
Carlotta Tagliaro <carlotta.tagliaro@gmail.com>
2018-11-03 15:37:55 +0100

    sql to insert Music products finished

--- insert_query/Finished/qry_musical-instrument .sql
+++ insert_query/Finished/qry_musical-instrument .sql
@@ -1,23 +1,29 @@
 INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Percussion Musical Instruments Set for Kids, 25 Pcs", "Kids can have fun finding rhythm and melody in everyday life. Musical Instruments Set can enrich their language, social development, cognition, attention span, mathematical, motor and self-regulation skills.", "", "Music", "g.s@agg.it");
-INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT LAST_INSERT_ID(), 'imagesUpload/51G1Yl+T+2L._AC_US218_.jpg');
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT Max(ID) FROM cucciolo.Prodotti, 'imagesUpload/51G1Yl+T+2L._AC_US218_.jpg');
 
 INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Adjustable Black Orchestral Musical Sheet Holder Tripod Stand", "Is perfect for use within home, stage or studio. Made from durable metal with a non-reflective black finish. The stand is Height and Angle Adjustable, and ideal for several pages of Sheet Music.", "", "Music", "g.s@agg.it");
-INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT LAST_INSERT_ID(), 'imagesUpload/31p2hNRiZVL._AC_US218_.jpg');
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT Max(ID) FROM cucciolo.Prodotti, 'imagesUpload/31p2hNRiZVL._AC_US218_.jpg');
 
 INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Martin Smith W-100 Acoustic Guitar", "The preferred choice for thousands of guitarists. Designed especially for students, this full size steel strung guitar comes complete with a strap, spare strings, plectrums and free online lessons. The body is perfectly contoured for a comfortable playing position.", "", "Music", "g.s@agg.it");
-INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT LAST_INSERT_ID(), 'imagesUpload/410+DBBuMoL._AC_US218_.jpg');
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT Max(ID) FROM cucciolo.Prodotti, 'imagesUpload/410+DBBuMoL._AC_US218_.jpg');
 
 INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Full Size Electric Guitar Superkit", "Is the ideal musical package for children and adults who want to start learning to play right away. This standard size electric guitar can play a variety of distinct notes, making it great for musicians who want to try anything from light rock to heavy metal.", "", "Music", "g.s@agg.it");
-INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT LAST_INSERT_ID(), 'imagesUpload/41HMbMKnSSL._AC_US218_.jpg');
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT Max(ID) FROM cucciolo.Prodotti, 'imagesUpload/41HMbMKnSSL._AC_US218_.jpg');
 
 INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Guitar Basics", "It is full of effective graphics, songs and clapping games, jokes that work and well-known movie and rock tunes (some using tab). The book, which is attractively illustrated and moves with ease through the lessons, getting to grips with rhythm and then simple notation, building to the beginnings of repertoire, featuring original and selected tunes.", "", "Music", "g.s@agg.it");
-INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT LAST_INSERT_ID(), 'imagesUpload/51loqg4MTLL._AC_US218_.jpg');
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT Max(ID) FROM cucciolo.Prodotti, 'imagesUpload/51loqg4MTLL._AC_US218_.jpg');
 
 INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("54-Key Portable Digital Piano Keyboard with Music Stand", "The 54-Key Digital Piano Keyboard is perfect for aspiring pianists of all ages. This compact, portable, and high quality keyboard has built-in stereo speakers and a large, easy-to-read LCD screen, which serves as a great teaching tool.", "", "Music", "g.s@agg.it");
-INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT LAST_INSERT_ID(), 'imagesUpload/41MzEjh-jUL._AC_US218_.jpg');
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT Max(ID) FROM cucciolo.Prodotti, 'imagesUpload/41MzEjh-jUL._AC_US218_.jpg');
 
 INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Yamaha P-45 Digital Piano - Black", "The P-45 from Yamaha, one of the worlds leading musical instrument manufacturer, is the enhanced model in the P-Series range with a compact and portable design which makes it useful on the road, in the home or in the classroom.", "", "Music", "g.s@agg.it");
-INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT LAST_INSERT_ID(), 'imagesUpload/31hqWn0XYgL._AC_US218_.jpg');
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT Max(ID) FROM cucciolo.Prodotti, 'imagesUpload/31hqWn0XYgL._AC_US218_.jpg');
 
 INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Piano Time 1: Bk. 1", "The highly popular Piano Time series has undergone a full facelift. Free supplementary material for the series is also included.", "", "Music", "g.s@agg.it");
-INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT LAST_INSERT_ID(), 'imagesUpload/51SgFovOecL._AC_US218_.jpg');
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT Max(ID) FROM cucciolo.Prodotti, 'imagesUpload/51SgFovOecL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Windsor Full Size 4/4 Violin", "The Windsor violin is the perfect instrument for students. With a spruce top, ebony coloured tuning pegs and fingerboard and fine tuning adjusters, it produces a bright tone and comes complete with a wood/horsehair bow.", "", "Music", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT Max(ID) FROM cucciolo.Prodotti, 'imagesUpload/51QKxCkgbzL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Odyssey OFL100 Flute Outfit - Black", "At Odyssey, we recognise the significant contribution that an instrument makes to the willingness of a student to persevere, practice, pursue the process of learning to control and master the instrument, enjoy making music and progress.", "", "Music", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES(SELECT Max(ID) FROM cucciolo.Prodotti, 'imagesUpload/41umHcuS1QL._AC_US218_.jpg');
