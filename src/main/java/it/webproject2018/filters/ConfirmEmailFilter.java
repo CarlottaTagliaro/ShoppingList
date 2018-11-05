7c588bf4893dc82cdfd2740eec5c19351e381420
caramellaio <albertobombardelli123@gmail.com>
2018-11-05 15:35:52 +0100

    Add emailconfirmation page's filter (page has not being added yet)

---
+++ src/main/java/it/webproject2018/filters/ConfirmEmailFilter.java
@@ -0,0 +1,41 @@
+package it.webproject2018.filters;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Alberto
+ */
+public class ConfirmEmailFilter implements Filter {
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
+        try {
+            HttpServletRequest req = (HttpServletRequest) request;
+            HttpServletResponse res = (HttpServletResponse) response;
+
+            if (req.getSession().getAttribute("verify") == null)
+                res.sendRedirect(req.getContextPath() + "/home");
+            else
+                chain.doFilter(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
