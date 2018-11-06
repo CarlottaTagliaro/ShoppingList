6a17bdbb6f19ea26ac9929caa1a7f70bf3c13e75
Carlotta Tagliaro <carlotta.tagliaro@gmail.com>
2018-11-06 18:55:21 +0100

    Sport Clothes

---
+++ insert_query/Finished/qry_sport_clothes.sql
@@ -0,0 +1,30 @@
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Women's 3 Pack Compression Dry Fit Vest Tank Top", "Neleus was born in 2008 ,with its unique design concept, excellent quality and comfortable clipping, has gain the crazy love from the clients. As the most energetic brand, Neleus is committed to provide the most innovative and price-friendly sport goods.","", "Sport Clothes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41a-13x2UsL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Ultrasport Women's Functional Running Pants Windprotect", "1 x Pant with Windbreaker, Compression Effect and Quick-Dry Function", "", "Sport Clothes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31nB+TM8DuL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Women's Tank Tops Running Vest Sleeveless", "Size Chart: ★Racer Back Tank Tops S/UK8/EU36: Bust: 81.4cm/32'', Length: 60cm/23.6'', Hem: 96.6cm/38", "", "Sport Clothes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41+SGAnEzDL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("TrailHeads Race Day Performance Running Cap", "The Lightweight, Quick Dry, Sport Cap for Women - 4 Colours", "", "Sport Clothes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51789l-u6QL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Men's Sweatpants Open Hem Lightweight Jogging", "Men's Athletic Pants Lightweight Joggers Pants Product features:Light-weight & breathable material dries quickly, keeping you cool and comfortable all day long.", "", "Sport Clothes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41ydDkPUyJL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Under Armour Mirage 8'' Men's Short", "These shorts are built for maximum freedom. They are lightweight, made to stretch and designed to let you move.", "", "Sport Clothes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41DM54FVjcL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Nike Mens Park VI T-Shirt", "The Nike Park VI jersey ensures maximum breathability through its Dri-Fit mesh inserts on the side. The Dri-Fit material directs sweat away from the skin and thereby creates a pleasantly cool and dry comfort for the wearer.", "", "Sport Clothes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41egExLNswL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Ultrasport Women's Professional Mila Ski Down Jacket", "The Ultrasport Professional Women's Winter Sports/Outdoor Down Jacket Mila is the perfect companion in colder temperatures. It shines with its many practical and clever functions.", "", "Sport Clothes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41IAmYM81cL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Suddora Kids Head Sweatband - Athletic Cotton", "Child Headbands Available in 18 different colors. Head sweatbands are great to use during basketball, running, cycling and other sports. We make our headbands with a ultra high quality material so it doesn't irritate your head.", "", "Sport Clothes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/3134NG2WzsL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Pocket Mens Shorts Leisure Running Gym Shorts", "The Yuerlian are the ultimate in performance for those who are passionate about sports and aim for the highest level.These specially designed feature everything you are looking for.", "", "Sport Clothes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41npYd6BTUL._AC_US218_.jpg');
