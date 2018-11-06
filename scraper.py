69ec7168fec521bf1611acf4954e9a950c16a4bb
Davide <davide.zanella10@gmail.com>
2018-11-06 16:49:51 +0100

    scraper partial fix

--- scraper.py
+++ scraper.py
@@ -5,30 +5,38 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import urllib
+import time
 
 url = "https://www.amazon.co.uk/s/field-keywords="
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
 
 def getProducts(cat):
     c_url = url + urllib.parse.quote(cat)
-    page = requests.get(c_url)
-
+    time.sleep(2)
+    page = requests.get(c_url, headers = headers)
+    
     soup = BeautifulSoup(page.text, 'lxml')
 
 
-    for img in soup.select('div.a-fixed-left-grid-inner img'):
-
-        pageDesc = requests.get(img.parent['href'])
+    for img in soup.select('div.a-fixed-left-grid-inner img.s-access-image.cfMarker'):
+        url_prod = img.parent['href']
+        if url_prod[:8] != 'https://':
+            url_prod = "https://www.amazon.co.uk/" + img.parent['href']
+        time.sleep(2)
+        pageDesc = requests.get(url_prod, headers = headers)
         soupDesc = BeautifulSoup(pageDesc.text, 'lxml')
         try:
             desc = soupDesc.select_one("#productDescription p").text
-        except:
-            print("exception")
+        except Exception as e:
+            print(e)
             continue
 
         nome = img['alt']
         src = img['src']
 
-        response = requests.get(src)
+        time.sleep(2)
+        response = requests.get(src, headers = headers)
         if response.status_code == 200:
             with open("images/" + os.path.basename(src), 'wb') as f:
                 f.write(response.content)
