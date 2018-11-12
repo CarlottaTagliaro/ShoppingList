79c8010051c623b363f934d92b8c9dbd730116b4
caramellaio <albertobombardelli123@gmail.com>
2018-11-12 20:56:34 +0100

    move EmailConfirmationServlet + minor changes

--- src/main/java/it/webproject2018/servlets/pages/EmailConfirmationServlet.java
+++ src/main/java/it/webproject2018/servlets/pages/EmailConfirmationServlet.java
@@ -3,18 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package it.webproject2018.servlets;
+package it.webproject2018.servlets.pages;
 
+import it.webproject2018.db.daos.UtenteDAO;
 import it.webproject2018.db.daos.jdbc.JDBCUtenteDAO;
 import it.webproject2018.db.entities.Utente;
 import it.webproject2018.db.exceptions.DAOException;
 import it.webproject2018.db.exceptions.DAOFactoryException;
 import it.webproject2018.db.factories.DAOFactory;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,17 +24,20 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author alberto
  */
+@WebServlet(name = "EmailConfirmationServlet", urlPatterns = {"/EmailConfirmationServlet.jsp"})
 public class EmailConfirmationServlet extends HttpServlet {
     
   
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException, DAOFactoryException {
         String hashStr = request.getParameter("verify");
+                        
         if (this.handleVerifyString(hashStr)) {
             response.sendRedirect(request.getContextPath() + "/login.jsp");
         }
         else {
             /* TODO[Alberto]: add a proper error */
+            System.out.println("unable to verify user");
             response.sendRedirect(request.getContextPath() + "/login.jsp");
         }
     }
@@ -48,8 +52,18 @@ public class EmailConfirmationServlet extends HttpServlet {
         }
     }
     
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (DAOFactoryException ex) {
+            Logger.getLogger(EmailConfirmationServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }    
+    }
+    
     protected boolean handleVerifyString(String verifyString) throws ServletException {
-        JDBCUtenteDAO jdbcUserDao;
+        UtenteDAO jdbcUserDao;
         DAOFactory daoFactory;
         
         daoFactory = (DAOFactory) super.getServletContext().getAttribute("daoFactory");
@@ -59,7 +73,7 @@ public class EmailConfirmationServlet extends HttpServlet {
         }
         
         try {
-            jdbcUserDao = daoFactory.getDAO(JDBCUtenteDAO.class);
+            jdbcUserDao = daoFactory.getDAO(UtenteDAO.class);
         } catch (DAOFactoryException ex) {
             throw new ServletException("Unable to check verify string, daoFactory could not get UtenteDAO", ex);
         }
