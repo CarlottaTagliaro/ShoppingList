2a3fc24930697872317f8faaa5c9c353ec14934d
Carlotta Tagliaro <carlotta.tagliaro@gmail.com>
2018-12-05 16:22:33 +0100

    added vegetables - KING BANANA

---
+++ insert_query/Finished/qry_king banana vegetables.sql
@@ -0,0 +1,29 @@
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Banana King Aubergines - 500g", "Banana King Aubergines - 500g", "", "Vegetables", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/11MUy2RuS2L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Banana King Mixed Peppers Salad - 4 Pack", "One of each of the above peppers in a pack", "", "Vegetables", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/11JYVMxPuaL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Banana King Cucumber Salad - Half", "Banana King Cucumber Salad - Half", "", "Vegetables", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/11I0byGLUPL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Banana King Red Onions - 500g", "Great for salads", "", "Vegetables", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/11M3v4ZEiCL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Banana King Washed Bakers Potatoes - 2Kg", "Washed baking Size potatoes", "", "Vegetables", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/11WkQ9GFGJL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Banana King Breakfast Mushrooms - 200g", "Large open cup mushrooms", "", "Vegetables", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/11IK2cYfIcL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Banana King Vine Tomatoes Salad - 500g", "Large Sweet tomatoes on the vine", "", "Vegetables", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/114wchJM1tL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Banana King Cherry Vine Tomatoes Salad - Pack", "Small tomatoes on the vine", "", "Vegetables", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/11JraIpE+aL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Banana King Celery Salad - Per head", "Washed and trimmed heads", "", "Vegetables", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/11C3q9s0PYL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("500g Fresh Natural Washed Carrots Vegetable", "Banana King Washed Carrots - 500gPACK", "", "Vegetables", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/11PFuytrRQL._AC_US218_.jpg');
