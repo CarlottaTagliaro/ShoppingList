89f2b7437efef2b7116a09ad5292fbe39afbf208
CarlottaTagliaro <carlotta.tagliaro@gmail.com>
2018-12-15 19:44:45 +0100

    added meat category

---
+++ insert_query/Finished/qry_steak.sql
@@ -0,0 +1,32 @@
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Meatpacks Steak Lover Pack", "This pack is perfect for the steak lover as it is full of some of the finest British Prime Beef Steaks. The steaks are two to a pack and they are so tender and full of flavour, plus you will save an additional 15% over buying them individually.", "", "Meat", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41SBN7oeFmL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("MS Traditional Pork Scratchings 45g (Pack of 12 x 45g)", "MS Traditional Black Country Pork Scratchings 45g ( Pack of 12 x 45g)", "", "Meat", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51yre7bh1UL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("A Real Traditional Pork chop", "A real traditional chop of meat, smoked hot, from the best traditional smokehouse in Poland of the Green Lungs of Poland, produced in an old home manner without the use of any preservatives or chemicals, the traditional way, with the highest quality raw material.", "", "Meat", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/412cAp1QvML._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Hook'N'Block Roast Selection Pack", "This Roast Selection Pack is around 25% cheaper than you would pay in the high street supermarkets. It contains 5 different roast joints.", "", "Meat", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41Ju+QzxagL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Meat and Sausage selection box", "A selection of some of our finest products designed to tempt and tantalise.", "", "Meat", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/61JIZCwtrJL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Graig Farm Organics Everyday Meat Box", "A selections of Organic meat for any occasion at a great price. 1 x 1.5 kg Whole Organic Chicken 1 x 640g Organic Chicken Thighs (approx 4) 1 x 1 kg Organic Pork Shoulder Roast (boneless) 1 x 400g Organic Pork Steaks ( 2 per pack) 1 x 1kg Organic Topside/Silverside Roast", "", "Meat", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/61XggYxFLbL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Hook'N'Block A Weeks Supply Meat Pack", "A weeks supply of meat for two, seven different meals in one meatpack!  2x Rump Steaks (min weight 400g) 2x Pork Loin Chops (min weight 400g) 2x Skinless Chicken Fillets (min weight 400g) 1x 500g pack of Lean Steak Mince 6x Cumberland Pork Sausages (min weight 454g)", "", "Meat", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/4167W8xZ-fL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Princes Corned Beef, 340g", "Princes Corned Beef is made with 100% beef. It is traditionally used in the curing process. The name ‘Corned Beef’ derives from the ‘corns’ of salt traditionally used in the curing process. It is ready to eat snack available in 340 gram can. Boil some potatoes, add milk and butter, and mash together with corned beef", "", "Meat", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/6105IEQWP4L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Meatpacks English Stewing Lamb 1kg Pack", "English Stewing Lamb.Sliced neck of English lamb ideal for stewing, illustrated by a hearty stew of lamb, green beans, onions, and new potatoes slow cooked to perfection.", "", "Meat", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41Vp-LW5daL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Meatpacks Boned & Rolled English Leg of Lamb 1.1kg", "This fresh deboned leg of English lamb is ready for the oven. So simple to carve and taste's delicious.Our English lamb comes from local farms and are raised using traditional natural farming methods.", "", "Meat", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51ZWFpnDhyL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Meatpacks English Carvery Leg of Lamb 2kg", "This English leg of lamb has a wonderful flavour coming from our grass fed local animals. This carvery leg is part boned and prepared so it is ready for roasting or pot-roasting. Perfect for that Sunday lunch as its simple to carve, and has a moist tasty flavour.", "", "Meat", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/413Ndkf2tRL._AC_US218_.jpg');
