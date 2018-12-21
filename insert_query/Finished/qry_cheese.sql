457c81aadb32d43afafc99490483dddd13c39008
CarlottaTagliaro <carlotta.tagliaro@gmail.com>
2018-12-21 16:03:38 +0100

    milk products (2 left)

---
+++ insert_query/Finished/qry_cheese.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("The Ultimate Snowdonia Cheese Co. Hamper", "The Snowdonia Cheese Company is famed for its deliciously moreish cheddar cheeses. This selection of truckles forms the ultimate hamper for any cheddar connoisseur. From the highly acclaimed classically creamy Black Bomber, to a wonderfully warm Amber Mist with smoky tones of Scotch Whisky, this cheese is hamper is bursting with character.", "", "Milk Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51r4FySXnFL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Comte Cheese | 500 Grams", "Delicious fresh from the knife Comte Cheese. ", "", "Milk Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51TWsLLk9kL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Snowdonia Cheese Company Hamper, 3 x 200g", "A lovely gift for the cheese lover in your life. Three of Snowdonia cheese company's finest cheeses.", "", "Milk Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51YMMNNVbSL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Nestlé Tip Top Cream, 250g", "Nestle tip top squirty cream is low in carbohydrates. It count as one of your 5 a day of fruit and vegetables. It has no artificial flavours, sweeteners or preservatives and is suitable for vegetarians. It contains 20 servings.", "", "Milk Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41mqsu103nL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Candia UHT Skimmed Milk, 6 x 1L", "Candia UHT Skimmed Milk contains the same amount of calcium as fresh milk, but keeps much, much longer. It has been heat-treated without the use of preservatives or additives. This makes it perfect as part of a healthy, balanced diet.", "", "Milk Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51PZFHqb8xL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Candia UHT Semi-Skimmed Milk, 6 x 1L", "Candia UHT Semi-Skimmed Milk contains the same amount of calcium as fresh milk, but keeps much, much longer. It has been heat-treated without the use of preservatives or additives. This makes it perfect as part of a healthy, balanced diet.", "", "Milk Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51GwmMEOynL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Lakeland Full Fat Milk Pots", "Giving you quick access to milk for use in your tea or coffee, these single use pots are perfect for catering and hospitality use. Featuring specially treated milk, this product is designed to last much longer than regular milk, preventing unwanted spoilage.", "", "Milk Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41WDcul4CZL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Dairy Crest UHT Semi-Skimmed Milk 500 ml", "Delicious semi-skimmed milk with just 2% fat for a guilt-free cup of tea or coffee. Supplied in a pack of twelve 500ml cartons, UHT milk is long life and can be stored at ambient temperature when unopened.", "", "Milk Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51vPnoen9RL._AC_US218_.jpg');
