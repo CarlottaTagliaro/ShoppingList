3fb64685334b2f9758ee0a155e3038ebd28d7ce6
CarlottaTagliaro <carlotta.tagliaro@gmail.com>
2018-12-26 21:35:21 +0100

    added sweets

---
+++ insert_query/Finished/qry_sweets.sql
@@ -0,0 +1,32 @@
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Pink and White Lollipops Jar Pack of 25", "Pink & White swirly lollipops come in a jar pack of 25 swirl lollipops. Our top-selling lollipop drums are jaw-dropping value. Each jar is stuffed with fruity flavoured sucka lollies.", "", "Sweets&Cakes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51JrQVdcneL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Sweets Red Gift Box With A Gold Bow", "Perfect, inexpensive treats which make stunning birthday, congratulations and get well soon presents. Go on surprise someone - they will love it.", "", "Sweets&Cakes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51-30m4yEkL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("HARIBO Giant Strawbs, 1kg", "Haribo Giant Strawbs strawberry flavour sweets. Suitable for vegetarians.  Includes 1 x 1056g tub.  Bursting with flavour and free from artificial colours too.  Perfect for sharing, parties, weddings, celebrations, gifts, Halloween or just as a treat.", "", "Sweets&Cakes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51bBi8x4sHL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("The Jelly Bean Factory Jar 700 g", "This 700 g jar of gourmet beans from The Jelly Bean Factory is packed full with 36 different flavours of delicious jelly beans, presented in a clear plastic jar with a re-sealable lid so that the beans can be kept fresh.", "", "Sweets&Cakes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51Bd7IfJfdL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Ultimate Jelly Sweet Bag Selection 1kg", "Perfect for those who simply cannot choose from our vast selection of sweets - or perhaps you want to try a 'little bit of everything'? Well this is for you.", "", "Sweets&Cakes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51xdDukvhYL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("5 Large Vintage Victorian Pick & Mix Sweet", "Victorian Style Plastic Jar Pick & Mix Sweet-Shop Kits Create a lovely vintage village fete atmosphere at your special occasion with our sweet-shop buffet kits! Ideal for weddings, christenings, birthdays and more.", "", "Sweets&Cakes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51YGSJZJN1L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Gourmet Chocolate Pizza Belgian Chocolate Pizza", "This delicious Happy Birthday Chocolate Pizza is sure to go down an absolute treat. Well, what chocolate-loving person wouldn't enjoy chomping down on a 7 Belgian chocolate base", "", "Sweets&Cakes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/61ZkbIg-DbL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("9 Divine Desserts Gift Box 225g", "The Jelly Bean Factory 9 Divine Desserts Gift Box. Flavours include; Banoffee Pie, Creme Brulee, Cookie Dough, Black Forest Gateau, Lemon Meringue Pie and more!", "", "Sweets&Cakes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41belcXk9qL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Betty Crocker Cream Cheese Style Icing", "Icing by Betty Crocker makes it easy to add a delicious finishing touch to your cake. Ready to spread with a knife or pipe into shapes for a smooth, creamy, and tasty cake topping. We also like using these between two cookies for an irresistible cookie", "", "Sweets&Cakes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41QNnfrrH+L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("American Candy - Bulk Sweets - 1.36KG", "Cow Tales Mini Vanilla Candies are an elongated version of Goetze's iconic sweets, Caramel Creams. Made with wholesome wheat flour, dairy milk, and cream ingredients.", "", "Sweets&Cakes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/619vDG+lugL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Jelly Bean Factory Carrying Jar", "Jelly Bean Factory Carrying Jar 1200g (1.2kg) 36 flavours", "", "Sweets&Cakes", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41kzQBv5KJL._AC_US218_.jpg');
