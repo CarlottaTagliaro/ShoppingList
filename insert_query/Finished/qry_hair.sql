db8719a25ccb0dd18f95d57a79b858528da78aab
CarlottaTagliaro <carlotta.tagliaro@gmail.com>
2018-12-27 20:55:13 +0100

    hair products

---
+++ insert_query/Finished/qry_hair.sql
@@ -0,0 +1,29 @@
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Extra-Large Wide-Toothed Shampoo Rake", "Extra-large shampoo rake that's ideal for combing product through hair to get even coverage. The wide teeth are gentle on the hair and help to detangle it. The comb is made from hardwearing black plastic and measures 244 mm.", "", "Hair Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41lg6sQiE+L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Hairdresser's Carbon Tail Comb", "Exceptional quality, tail comb with a long tail that's perfect for sectioning and fine teeth for combing. The Kobe Carbon Tail Comb is made with carbon fibre to give it strength and durability.", "", "Hair Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41Pip6sPEVL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Schwarzkopf Steel Silver Number 098", "Discover unmatched colour vibrancy thanks to built-in electrifying pigments. Schwarzkopf LIVE Color Ultra Brights is an HD intense colour. Live's HD-vibrant pigment technology intensively saturates each hair strand with high definition colour.", "", "Hair Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51j4uF6YmGL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("L'Oreal Natural Brown Hair Dye", "L'Oreal Paris hair colorant kit has an enhanced technology, enriched with three ingredients. It features excellence creme revitalises colour, hair feels stronger, enriched with ceramide.", "", "Hair Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51bghNsdPTL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Heat Protect Spray", "ghd Style Heat Protect Spray provides an invisible barrier against everyday heat damage on damp or dry hair. Containing the ghd Heat Protection System, this spray can be layered with other styling products to create the result you desire.", "", "Hair Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31cJzoIMi4L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Hairdressing Scissors 5.5 inch", "Very comfortable to work with, light-mid weight scissors with unique deep black colour which is appealing to the eye. Suitable for Professional as well as for personal use.", "", "Hair Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41UbQkXMVCL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Duo Shampoo & Conditioner -750 ml", "Continuously repairs with long lasting results, smoothes and adds shine, heat protective and colour safe. A perfect product to use at home or to fill up their travel bottles to take on a winter getaway.", "", "Hair Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51GuXiJHQhL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Anti Dandruff Shampoo and Conditioner", "A tried and tested anti-dandruff shampoo and conditioner, Head & Shoulders classic clean 2-in-1 keeps your hair looking and feeling flake-free. Balanced, clean hair starts with the scalp.", "", "Hair Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/4132DQxz7mL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("PROFESSIONAL Hair Treatment", "UniqONE ALL IN ONE is the brand that… - BELIEVES IN A NEW APPROACH TO HAIR CARE. - OFFERS 10 BENEFITS IN EACH PRODUCT TO ACHIEVE SPECTACULAR RESULTS. - USES THE MOST ADVANCED TECHNOLOGY.", "", "Hair Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31kEppZOYlL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Repairing Conditioner", "with Organic Jojoba Oil for Dry or Frizzy Hair | Soft, Smooth, Deep Moisture Product for Heat, Color or Bleach Damage | Detangling, Anti Breakage/JUSTICE Professional 300ml 10oz", "", "Hair Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31iokxzyttL._AC_US218_.jpg');
