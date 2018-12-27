58485ea596ac63197809856f6032b29fe5e759b0
CarlottaTagliaro <carlotta.tagliaro@gmail.com>
2018-12-27 08:59:33 +0100

    added makeup

---
+++ insert_query/Finished/qry_makeup.sql
@@ -0,0 +1,29 @@
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Million Lashes Mascara Black", "Millionize your lashes with L'Oréal Paris mascara: Volume Million Lashes, our iconic gold mascara. The Millionizer Brush has a multitude of bristles which separate lashes for a fanned-out effect. The elastomer applicator is ultra flexible with both long and short bristles to coat lashes.", "", "Make-Up", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31MMwn5YECL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Black Vinyl Liquid Eye Liner", "L'Oréal Paris Super Liner in Black Crystals is a liquid eyeliner in a special sponge tipped pen for the creation of fine lines or thick, sultry flicks. The intense black colour with shimmering sparkle is applied with precision and ease and dries without smudging.", "", "Make-Up", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/21EZuFk8glL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Exaggerate Waterproof Liquid Eyeliner", "Rimmel exaggerate waterproof liquid eye liner are intense water resistant colour, utmost control, draw the lines you want. Highly pigmented liquid formula delivers a bold eye liner with glossy finish. Maximum control applicator helps create a smooth and precise line around the eye contour.", "", "Make-Up", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31nyaWm1MJL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Clarins Instant Concealer Corrector", "Original and exclusive beauty and personal care products by Clarins", "", "Make-Up", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31F06J-+4pL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Lasting Perfection Wear Concealer", "COLLECTION's Lasting Perfection Concealer is a multiple award-winning concealer, known for concealing blemishes and imperfections for up to 16 hours. The formula is water-resistant, transfer-proof and easy to blend. It can create a flawless base either used alone or with foundation.", "", "Make-Up", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/314GUnp162L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Super Lustrous Lipstick", "Revlon Super Lustrous Lipstick has a classic silky smooth finish. It features silky smooth, moisturising colour, liquisilk formula seals in colour and softness, dazzling array of fabulous, fashionable shades. It is available in pink in the afternoon shade.", "", "Make-Up", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31aiDalv20L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("iDivine Eyeshadow Palette A New Day", "Your makeup should work (and play) as hard as you do, and these two new i-Divine eyeshadow palettes from Sleek MakeUP will carry you through from a day at the office to a night on the town. Both palettes are super-versatile and contain 12 shades of our iconic formula eye-shadow.", "", "Make-Up", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31bUIIemOXL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Pup Fard Lumin", "Pupa Like a Doll Luminys Blush Pure color for an extraordinary light effect! Soft and silky texture - rich in precious ultra fine pearls - tones cheeks and cheeks, enhancing them with incredibly radiant shades. For a true make-up artist result, use the Blush Obliquo and Round brushes.", "", "Make-Up", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31zBld+mD7L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("iDivine Eyeshadow Palette Storm", "Sleek MakeUP’s legendary i-Divine palettes are 12 shades of super-pigmented eyeshadow, suited to every skin tone. The colour payoff is excellent and the high-quality formulation is long lasting. Get creative and blend contrasting colours together or mix shimmers with matte shades.", "", "Make-Up", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41npScvXFhL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("120 Colours Eyeshadow Palette", "A variation of 120 colours including neutral, vibrant, shimmer and speckled arranged in a palette formation so it is easy to view. The palette is in a sleek and stylish black case so you can take your eyeshadow almost anywhere! Whether you are going for a darker and more mysterious look or bright and bold.", "", "Make-Up", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51Hm25BC21L._AC_US218_.jpg');
