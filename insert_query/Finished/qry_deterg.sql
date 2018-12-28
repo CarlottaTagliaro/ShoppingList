3702c2530fe2a5e2fc227edbbd1d51588bb2d749
CarlottaTagliaro <carlotta.tagliaro@gmail.com>
2018-12-28 10:29:21 +0100

    detergent

---
+++ insert_query/Finished/qry_deterg.sql
@@ -0,0 +1,29 @@
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Pure Concentrate Fabric Conditioner", "Comfort pure hypoallergenic fabric conditioner 3 Litre Comfort pure hypoallergenic fabric conditioner 3 Litre keeps clothes soft and is gentle on skin. This gentle yet effective fabric conditioner for sensitive skin is dermatologically tested and ideal for your baby's skin.", "", "Detergent", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/410jYTlBycL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Ecover Zero Laundry Liquid", "Ecover zero laundry liquid is a fragrance free laundry detergent that works on your clothes and is kind to your skin. It is perfect for sensitive skin and approved by allergy UK. It has no nasties, gentle plant based and mineral ingredients and also suitable for septic tanks.", "", "Detergent", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41NcJ9ezk7L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Small & Mighty Colour Washing Liquid", "When you're trying to put the laundry on in a rush, its easy to get detergent everywhere. Persil colour liquid detergent has a specially designed bottle and ball to help minimize spills. Persil formula combines wash booster and colour protect technology to give you powerful stain removal, without letting colours fade.", "", "Detergent", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41rof2jsDTL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Ariel Biological Liquitabs", "Extraordinary cleaning power in one little pod. Ariel 3-in-1 pods washing capsules have a distinct multi-compartment design that keeps the ingredients stable and separate until they reach the wash, providing a super concentrated cleaning power. The innovative film of Ariel washing Tablets dissolves completely in contact with water.", "", "Detergent", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51ocDL-2czL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Powercaps Bio Washing Capsules", "For cleaning power, even in a quick lower-temperature wash, try Persil power caps washing capsules. The triple action formula in these Persil bio capsules gives you powerful stain removal and a fresh fragrance, all while caring for the colours and fibres in your clothes.", "", "Detergent", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51JCh4RVcjL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Fairy Clean & Fresh Washing Up Liquid", "Fairy Clean and Fresh washing up liquid gives you sparkling clean dishes and its revolutionary formula infuses your kitchen with light, clean, fresh scents from around your sink.", "", "Detergent", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41W8bIlUZwL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Fairy Non Bio Washing Powder", "Nothing beats the gentle touch of Fairy for powerful cleaning that feels great next to your skin! Ideal for children and those with delicate skin, it really is the ultimate washing detergent. Containing enough powder for 90 Washes, this is a great value way to get the most for your money.", "", "Detergent", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51DMqFoexUL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Surf and Comfort Fragrance Laundry", "Want to add a fun, fragrant spin to a laundry day? Surf, the number one fragrance laundry detergent brand, together with comfort, the number one fabric conditioner brand have created an exciting essential fragrance laundry kit. This collection of elegant, intense and long-lasting fragrances starts with Surf tropical lily and ylang washing liquid.", "", "Detergent", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51pAHby1uHL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Ariel Washing Powder Original", "Gives You Outstanding Stain Removal in The First Wash, 4.225 kg, 65 Washes", "", "Detergent", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51tKRJkj93L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Mr Muscle Window and Glass Cleaner", "Mr Muscle 5-in-1 window and glass cleaner is scientifically formulated with 5-in-1 technology, giving you the power to defeat tough messes on windows and glass. It removes tough grease and grime and leaves surfaces with a streak-free and smear-free shine. It is use on windows, mirrors and other glass surfaces.", "", "Detergent", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/410Ezy3iDwL._AC_US218_.jpg');
