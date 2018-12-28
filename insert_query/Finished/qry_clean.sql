517f5d050fc656196165dd1c7c51c3e41166e34d
CarlottaTagliaro <carlotta.tagliaro@gmail.com>
2018-12-28 09:53:00 +0100

    cleaning supplies

---
+++ insert_query/Finished/qry_clean.sql
@@ -0,0 +1,29 @@
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Faithfull Brooms with Handles", "The Faithfull Soft Broom is a low cost plastic broom with soft PVC bristles complete with a tubular steel, thread fitting handle. With a handy hanging bracket. Width: 300mm.", "", "Cleaning Supplies", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31eilDcBy9L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Anti-Dust Broom Plus Dustpan Set", "By combining two sweeping materials in the broom head, foam on one side and bristles on the other, the Vileda DuActiva broom effectively gathers dust and dirt in just one sweep, making sweeping up quicker and easier.", "", "Cleaning Supplies", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41c1eDU6maL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("RoadtoH Broom and Dustpan", "This long handle broom and dustpan with 36.9inches and 47.1 inches adjustable long handle for those person with backache.You will never bend over even you are very tall.There is extended poles,your tall husband or your tall handsome boy will be very willing to help you do cleaning and will never bend your back.", "", "Cleaning Supplies", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41ebkoK+GSL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Broom Plus Long Handled Dustpan", "The Vileda 3action broom and long handled dustpan make sweeping up less of a chore and saves you having to bend down. The broom has three different types of bristle, to sweep up fine dust, larger particles and to clean into corners, while the rubber bumpers help prevent damage to furniture.", "", "Cleaning Supplies", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41ef9DGt0VL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Beldray Turquoise 5 Piece Cleaning Set", "Keep your home spotless with this fantastic Turquoise Beldray five piece cleaning set consisting of a steel broom, dustpan and brush, scrubbing brush and a dishwashing brush. Made from plastic with soft grip handles, the tough brush bristles provide effective cleaning without being abrasive.", "", "Cleaning Supplies", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41N6BtY7RiL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Cleaning Set with Mop, Duster and Broom Heads", "Discover a more efficient way to clean your home with this fantastic Beldray click and connect cleaning set. Made from strong steel, the telescopic handle extends up to 1.2 metres so you can reach further into corners and underneath furniture. With fantastic mop, duster and broom heads included.", "", "Cleaning Supplies", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41obcFCfVkL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Microfibre Cleaning Towel/Cloth", "Zoegate microfiber towel Lint-free Dual Layer Ultra-Thick Car Wash Buffing Wax Polishing and Drying Cloth Towel Auto Detailing Towels 40 x 45 cm - perfect for clean", "", "Cleaning Supplies", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51S5t8NV6jL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Cotton Mop with Free Refill", "Cotton Mop (with Free Refill) in Graphite. Simply twist the current head off and replace with your new mop head. Simply take off the packaging , loosen the strands and screw the head into position. The Addis mop refills come with the Addis guarantee of quality.", "", "Cleaning Supplies", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31tV4-ydW5L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Hoover Freedom Cordless Stick Vacuum Cleaner", "Freedom offers convenient cordless cleaning throughout your home, with powerful 22v lithium performance. The rotating brush bar works wonders on your carpets, whilst the removable handheld helps you to tackle stairs and other tricky spots. Features include continuous power mode.", "", "Cleaning Supplies", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31l07Pmn79L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Hoover Bagless Cylinder Vacuum Cleaner", "Hoover Whirlwind SE71_WR01 Cylinder Vacuum Cleaner is rated ‘A’ for energy and for hard floor performance. It is suitable for all types of floors and includes a 2-in-1 dusting brush and crevice tool for effective cleaning of a wide range of surfaces, nooks and crannies.", "", "Cleaning Supplies", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41XDtKq5NzL._AC_US218_.jpg');
