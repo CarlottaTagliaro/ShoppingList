46910b95b8e5d209e0655215b5b6ba1030dca158
CarlottaTagliaro <carlotta.tagliaro@gmail.com>
2018-12-28 09:09:57 +0100

    body products

---
+++ insert_query/Finished/qry_body.sql
@@ -0,0 +1,35 @@
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Unbleached Natural Sea Sponge", "Strong and durable - suitable for both children and adults, for use in bathing, cleansing, exfoliating and applying cosmetics", "", "Body Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51zwQGBKL9L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Pack of 12 Exfoliating sponges", "oft synthetic sponges with exfoliating sides", "", "Body Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/21KY1nfEdYL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Dove Silky Body Cream Pot", "It's made with a luxurious combination of delicate skin softeners and nourishing ingredients. Dove silky nourishment body cream wraps your skin in silky moisture, leaving it radiant and soft to the touch every day. It contains the deep care complex with natural skin nutrients and rich essential oil to deeply nourish skin.", "", "Body Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31bSGKBzRML._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Body Lotion with Nordic Berry", "Dry to very dry skin it enhances skin's hydration level by three times and makes soft and nourished skin. Combination of moisturisers and Nordic Berry, clinically proven to work with skin's natural processes to enhance its hydration.", "", "Body Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31rSPHrpWFL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Green Tea Bamboo Honey Drops Body Cream", "Elizabeth Arden green tea bamboo honey drops super moisturizing body cream is all-over body cream infused with a blend of real honey and fragrant green tea extract helps to calm and condition dry skin. The fragrance that energizes the body, excites the senses and invigorates the spirit.", "", "Body Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41aVoG2aiWL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Shea Butter Body Lotion Dry Skin", "When you apply a moisturiser, how long does it take before your skin stops feeling hydrated? Garnier takes care of you with: Intensive 7 Days Ultra-Replenishing Lotion. Very dry skin formula enriched with: L-bifidus. It has a moisture replenishing anti-evaporation active.", "", "Body Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41a9-5RZ0SL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Wild Fig & Grape - Hand & Body Lotion", "A rich & indulgent hand & body lotion blended with a mouth watering blend of grape & fig oils and extract. Apply all over the body after a bath or shower to leave your skin smooth and nourished.", "", "Body Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51Jw+O69dKL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Glycolic Fix Body Cream", "Moisturising + refining body lotion perfect for flaky or oily skin. Powerful glycolic acid combines with salicylic + lactic acids to naturally exfoliate + retexture, revealing instantly smoother + brighter looking skin.", "", "Body Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/413iN0RVf9L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("6 Piece Body Wash Soap Bar Collection", "If you are looking for the perfect gift for yourself or someone close to you, then you are in the right place. Why have one when you can have it all with our 6 piece soap bar collection!Our soaps are lightly scented with a perfect blend of essential oils for your skin and body.", "", "Body Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51qd5rGTYSL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Cocoa Butter Skin Therapy Oil", "Palmer's Skin Therapy Oil is an advanced multi-purpose skin perfecting light-weight "dry" oil that can be used all over the body, including the face. It’s an all-in-one skincare solution.", "", "Body Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41vyJ3mwC4L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Nail and Cuticle Therapy, 15 g", "Get glossy, healthy looking nails with La Source nail and cuticle therapy. Features our restorative marine blend of anti-oxidant rich green seaweed extract and protecting red sea algae. Shea butter and glycerine to softens and moisturises. Panthenol promotes strong, healthy nails, prevents breaking.", "", "Body Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41RZiQYQa+L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Britney Spears Eau de Perfume", "Behind Britney's bright star is an artist of uncompromised dedication who knows that the greatest shows are created before the lights come on. Private Show Britney Spears is a portrait of Britney's passion as an iconic performer and the essence of her raw talent as a dancer. It's Britney in her element.", "", "Body Products", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31I+5uhYo-L._AC_US218_.jpg');
