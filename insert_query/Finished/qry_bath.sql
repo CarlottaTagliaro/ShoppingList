3821d5b5d75675400317338557b60c8a93197a34
CarlottaTagliaro <carlotta.tagliaro@gmail.com>
2019-01-03 11:09:03 +0100

    added bathroom

---
+++ insert_query/Finished/qry_bath.sql
@@ -0,0 +1,32 @@
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Sink Bathroom Storage Cabinet", "Beautiful Slatted Doors With Internal Shelves to Ensure storage for all Bathroom Storage;Finished in an Elegant Shade White that will perfectlt go with your bathroom;Material - MDF;Space Saving Storage Solution for any Bathroom;", "", "Bathroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31iN2suMLRL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Basin Wall Mounted Hung Bathroom Sink", "The basin is the most frequently used fixture in any bathroom; it is the focal point of the room, from which the entire atmosphere and personality of the room is built. As such an important piece of sanitaryware, it is vital to have a basin which is not only functional.", "", "Bathroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41EXvqF3rYL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Adjustable Bathroom Organiser Corner", "Handy storage for bathroom toiletries fed up with leaking bottles on the floor of your shower? Want more space to store toiletries? Our 4 tier adjustable bathroom organiser gives you 4 shelves of storage in one.", "", "Bathroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51AYJsAnQGL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Cotton Twist Bath and Pedestal", "This practical and easy-care bathroom collection has been created by talented British designers. Using the very latest trends, colours and manufacturing technologies, this bath mat set provides utmost style and durability for any home.", "", "Bathroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/61xhbsQZKYL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Bench Storage", "Paulownia wood furniture (known for its high stability and durability, resistant to insects and mould). The paulownia stem has a wood of remarkable lightness (from 300 to 400 kg/mc) and extraordinary dimensional stability. ALL OUR FURNITURE ARE PAULOLONIA WOOD", "", "Bathroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41G9vCKlILL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Straight Shower Bath Glass", "Sliding Screen Bathtub Square Design Enclosure", "", "Bathroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51b1NYNfjPL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Bath Straight Bathtub Shower Screen", "1600 x 700 Designer Square Single Ended", "", "Bathroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/518-TCFSvuL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Maison & White Frameless Rectangle Mirror", "The Maison & White frameless mirror is designed for use in any room at home or at work. The mirror can be fixed to the wall in landscape or portrait orientation making it great for a range of uses/rooms. The mirror is 4mm thick so it mounts flush to the wall.", "", "Bathroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31jzmZQMBSL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Makeup Mirror Double Sided", "DescriptionThis item is manufactured with premium material for durable and long-lasting use. The rotary structure with double sided design will ensure its practicability and popularity as a makeup mirror, because it will make your makeup process more easily and clearly by its 1:2 magnification.", "", "Bathroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31ZAawK6gXL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Bathroom Thermostatic Mixer", "Micoe Bathroom Thermostatic Mixer Shower Valve with 8 Shower Head and Hand Held Shower system M-A10427-1D", "", "Bathroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31paBlQR5TL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("VeeBath Base Modern Ceramic Close Coupled Toilet", "SKU:-- PRI002Product Range:-- BaseHeight:-- 748mmWidth: -- 382mmDepth:-- 630mmWeight:-- 26.5 KgType:-- Close Coupled ToiletMaterial:-- CeramicFinish:-- WhiteFlush Type: -- Dual FlushToilet Seat:-- Normal SeatMounting:-- Floor MountedPackage", "", "Bathroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31SH0K8UjRL._AC_US218_.jpg');
