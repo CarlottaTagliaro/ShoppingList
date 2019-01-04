f12a1a1e5a303f7be072146bedf5ae2328843aec
CarlottaTagliaro <carlotta.tagliaro@gmail.com>
2019-01-04 22:54:25 +0100

    added living room

---
+++ insert_query/Finished/qry_living.sql
@@ -0,0 +1,26 @@
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Living Room Armchair with Solid Wooded Legs", "WarmieHomy Soft Velvet Occasional Chair for Living Room Bedroom Hallway Office ConservatoryProduct Specification:Total Chair Dimensions: 84 x 77 x 102 cm (W x D x H)Backrest Height: 67cmArmrest width: 15cmSeater Size: 48 x 50cm (W x D).", "", "Living Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/519UE6U1GbL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Shaggy Rug Long Pile Carpet", "Shaggy rug, long pile design, multi-coloured. Material: 100% heat-set polypropylene. Manufacturing: machine woven. Lifting height: -30 mm. Weight/m² in kg: 2.250.", "", "Living Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/61rXv64mbDL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Couch L Shaped 3 Seater Sofa Settee", "This chic of European style will fit the decoration style of your living room,upholstered in a soft cotton style durable polyester fabric. This beautiful corner sofa has a reversible chaise so can be set up left side,but also can be right side which is lovely touched to your heart.", "", "Living Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/5164SjyakpL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Standing Pedestal Stand Fan Adjustable", "Black 16 Standing Pedestal Stand Fan Adjustable Oscillating Rotating Stay Cool 3 Speed", "", "Living Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41S83WsRNjL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Floor Lamp LED Eye-Care Standing Lamp", "  Famirosa LED Eye-Care Standing Lamp Remote Control Floor Lamp  Advantage of Our Best-selling New Generation Daylight Floor Lamp: 1 .Our Remote Control Floor Lamp is original and professionally customer-made which with much more better quality.", "", "Living Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/311SPL9xg4L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("IKEA Floor Uplighter Light Lamp", "Pack of 1Stylish and functionalOriginal IKEA productHeight: 175 cm, Base diameter: 26 cm, Shade diameter: 18 cm, Cord length: 1.9 mBulb not included", "", "Living Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/21kuNwqzdAL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Wall Clock Metal Frame Glass Cover", "Sleek and sample, the wall clock is distinguished by its modern and sleek quiet sweep design. A simple yet bold number look with a silver mental frame and a traditionally circular silhouette. The quartz movement helps you keep track of your day more accurately.", "", "Living Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/414oXC13m3L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("40-Inch 4K Ultra HD Smart TV", "Experience the beautifully realistic vibrancy of the 4K HDR picture with Samsung's NU7120. Stunningly clear 4K Ultra HD resolution combines with HDR powered by HDR10+ to achieve staggering levels of detail, brightness and contrast.", "", "Living Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51H1H9WDNPL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Panasonic 720p HD Ready 24-Inch LED TV", "24 LED HD Ready TV, with Freeview HD Tuner and 2 x HDMI inputs (1.4 ) and 1 x USB input (2.0 ) for easy connectivity.", "", "Living Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/419qeoAUwNL._AC_US218_.jpg');
