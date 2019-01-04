73ac591fa39249ff21e73d2e52bb58633686b1f8
CarlottaTagliaro <carlotta.tagliaro@gmail.com>
2019-01-04 21:07:31 +0100

    added kitchen

---
+++ insert_query/Finished/qry_kitchen.sql
@@ -0,0 +1,32 @@
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Pressure Cooker with Steamer Basket", "Inject a wealth of healthy eating habits into your home with the 6 L Stainless Steel Pressure Cooker. Boasting a capacious capacity and 22cm diameter, this pressure cooker is ideal for cooking delicious meals.", "", "Kitchen", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41a4NKXKLiL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Electric Pressure Cooker", "Pressure King Pro PKP5LCH 5L Electric Pressure Cooker, Stainless Steel, 900 W, 5 liters, Chrome", "", "Kitchen", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41EE+XfWJZL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Multi-Function Electric Cooker Pan", "The Benross Group, provider of quality items for your home and garden. This great multi-functional electrical cooker from the Quest brand is perfect for cooking for large families. A great cooking solution for smaller areas with less hassle and less mess.", "", "Kitchen", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51QxJZbypGL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("5 Piece Knife Block", "Give your kitchen a touch of contemporary style with this elegant five-piece knife set. Sturdily built from high-grade stainless steel, this set comprises a 20 cm bread knife, a 20 cm chef knife, a 20 cm slicing knife, a 12.5 cm utility knife and a 7.5 cm paring knife.", "", "Kitchen", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41Ua2lGWAXL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Steel Kitchen Sink Strainer Plug", "Package included: 1* Sink Strainer Plug Warranty: 60 days money-back & 24-months warranty & Within 24 hours response", "", "Kitchen", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/515U7pxcPHL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Single Bowl Steel Kitchen Sink", "KS-1001 Single Bowl Kitchen Sink with Drainer & Waste 1001", "", "Kitchen", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41GIIK2iknL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("White Ceramic Kitchen Sink & Waste Kit", "Stainless steel bowl basket (CWB15) available for this sink in our StoreDimensionsMinimum Cabinet Size - 600mmHeight: 24cmWidth: 101cmDepth: 53cmMain Bowl Height: 180mmMain Bowl Width: 330mmMain Bowl Depth: 410mmSecond Bowl Height: 150mmSecond Bowl Width: 155mmSecond bowl depth: 285mm", "", "Kitchen", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51B-29UT36L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Smart Oven Pro - Silver", "Tenderise and develop complex flavours with even convection heating from above and below using the Slow Cook settings from 4 to 10 hours and Keep Warm will automatically turn on after cooking is complete so it's ready to serve when you are.", "", "Kitchen", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41lLp46RYFL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Frying Pan Non-Stick Anti-Scratch", "The Benross Group, provider of quality items for your home and garden. The Blackmoor Home range of pans are ideal for if you are expanding or starting off a collection of high quality pans with a lower price tag.", "", "Kitchen", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/413wNcoIpAL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Twin Pack, Frying Pan Set", "This perfect set of frying pans offers you great value for money, not one frying pan, but two, ideal for any household. This set offers you stress-free cooking, the non-stick coating promises a durable non-stick cooking performance, whilst Tefal's patented Thermospot technology lets you know when the pan has reached the optimum cooking temperature.", "", "Kitchen", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41M9dBebHpL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Induction cooking pot with glass lid", "Tefal E79162 Jamie Oliver Induction cooking pot with glass lid, 24 cm in diameter, 6.7 L, suitable for induction, stainless steel", "", "Kitchen", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31hg4X4AYML._AC_US218_.jpg');
