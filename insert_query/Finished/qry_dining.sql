ee8638f03de1a91ab64b8258d2622cc0860cf847
CarlottaTagliaro <carlotta.tagliaro@gmail.com>
2019-01-04 22:19:00 +0100

    added dining room

---
+++ insert_query/Finished/qry_dining.sql
@@ -0,0 +1,30 @@
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Stretch Chair Seat Covers", "Stretch Chair Seat Covers for Dining Room, Beige Set of 4 Jacquard Dining Chair Seat Cushion Protectors Chair Slipcovers", "", "Dining Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41BZxEsZ3UL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Realistic Artificial Holland Tulip Flowers", "10pcs Realistic PU Artificial Holland Tulip Flowers", "", "Dining Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51xTDHXAqPL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Fake Flowers Bouquet Silk Tulip", "Beauty and elegance is exactly what you'll see when you gaze upon this striking fancy Artificial Tulip Flowers Bouquet. Perfect for Wedding Photography/Nice for home decoration.  Specification: Material: Silk Cloth, Plastic, Iron Wire", "", "Dining Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/514RBdlmjJL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("STUNNING GLASS BLACK DINING TABLE SET", "Transform and brighten your dining area with this stunning, contemporary dining table with faux leather chairs. Available in White or in Black The white set has clear tempered glass, a great style, especially when light is reflected on its surface and when the table is laid out for dinner.", "", "Dining Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51RK5f+YNvL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Millenium Table Fork", "Genware NEV-2000-2 Millenium Table Fork (Dozen). 195mm long.", "", "Dining Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31wh7O-s6+L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Grey kitchen table with acacia table top.", "Given that the kitchen table is often the most used piece of furniture in a home, we wanted to product a range that was aesthetically pleasing but was exceptionally hard wearing. The top is made from hard wood, acacia, and is treated with an oil guard, which is stain and heat resistant.", "", "Dining Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41GNg2FtldL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Elevate Knives, 6-Piece Set", "The Elevate range of kitchen tools has been specifically designed to improve hygiene and minimise the mess made by placing utensils onto kitchen surfaces during use. Each knife has an enhanced weighted handle with an integrated tool rest, ensuring that, when an Elevate knife is placed down, its head is always raised off of the work surface.", "", "Dining Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41d4OswRdyL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Millennium Tea Spoon", "Genware NEV-2000-8 Millennium Tea Spoon (Dozen). 140mm long", "", "Dining Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41Nz7sqY9DL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Stainless Steel Serving Spoons", "KitchenCraft MasterClass Stainless Steel Serving Spoons, Silver, 2-Piece", "", "Dining Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31bZMsEX5hL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Heavy Duty Folding Table", "Folding  Table  1.8  mtrs 750 cm width. Ideal for  Garden  Picnic  Table/Trestle  Table.Ideal for  Garden  Picnic  Table/Trestle  Table.Collapsible  and Compact when  Folded.", "", "Dining Room", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31oP+Dpu80L._AC_US218_.jpg');
