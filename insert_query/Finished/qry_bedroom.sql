0824e3580411cc45dd46dcf34daf41e88dc4ae7a
CarlottaTagliaro <carlotta.tagliaro@gmail.com>
2019-01-04 15:07:33 +0100

    added bedroom

---
+++ insert_query/Finished/qry_bedroom.sql
@@ -0,0 +1,31 @@
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("5ft King Size Memory Foam Mattress", "Mattressmakers memory mattress arrives topped with a dense layer of memory foam to comfortably hold the body's natural shape. supported bu a quality germain bonnell spring unit.", "", "Bedroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51p7BWZQw9L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Double Bed Upholstered in Faux Leather", "Best selling European designer LED bed. Ingenious design the Rio bed is a truly modern classic. This simply stunning bed creates the ultimate contemporary statement. The Rio LED comes with a unique multi - colour LED headboard.", "", "Bedroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51HA4KPfAVL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Double Plain Brushed Microfiber Bedding", "Size: double size | Color: Grey Box contains: double 1 x (200x200)cm duvet cover and 2 x (50x75)cm pillowcases  Tips: Machine wash with similar colors under 30 degree, no bleach, tumble dry low. Soft luxury bedding for an affordable priceMohap duvet cover set features in its plain and simple design.", "", "Bedroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51j+b9O4zUL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Easy Care Duvet Cover Set", "Stunning design and colouring brings the Italian Rivera to your bedroom. Marinella offers a colourful mulit panel patchwork face and bedspread with a bold baroque floral reverse.", "", "Bedroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/61-kSYO5AyL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Rectangle Mirror Wall Hanging", "Wall mounted 120 x 45cm unframed frameless bathroom mirror with 4 drilled holes. Wall hanging fixings included. This mirror is made from high quality 4mm silver mirror. The edges of the mirror are smooth.", "", "Bedroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/21PkD4avRCL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Furniture Budget 2 Door Wardrobe", "Mexican design solid pine furniture", "", "Bedroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41rIDLjRnmL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Bedroom Furniture-2 Door Wardrobe", "The Wardrobe from our Boldon Bedroom Furniture Range is stylish and affordable. This Robe is a nice size to store away all of your clothing in the bedroom. Comes in Black Ash effect finish with silver plastic handles.", "", "Bedroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/31yj7arIA3L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Portable Wardrobe for Hanging Clothes", "About Us  PREMAG portable wardrobe will help make your home neat, tidy. DIY assemble  make it meet various demands. It's also very easy to assemble and offers sturdy result.  Specification: 16 Cube Closet", "", "Bedroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/51+DhExE3PL._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Super Soft Bamboo Fiber Pillow 2 Pack", "For back sleeper provides comfortable support for back sleeper for side sleeper ultra-soft cover for face and extra help for neck for stomach sleeper breathable, warm and safe from allergy a good pillow is always necessary for a good sleep.", "", "Bedroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41W-xpiOO8L._AC_US218_.jpg');
+
+INSERT INTO Prodotti (Nome, Note, Logo, Categoria, Owner) VALUES("Silentnight Finesse Pillow", "Experience a good night's sleep with this premium finesse pillow from Silentnight. Providing soft/medium support for enhanced comfort with elastesse fabric ultimate cover for an air cushioned feel.", "", "Bedroom", "g.s@agg.it");
+INSERT INTO Prodotti_immagini (ID, Fotografia) VALUES((SELECT Max(ID) FROM cucciolo.Prodotti), 'imagesUpload/41I5HmEWUXL._AC_US218_.jpg');
+
+
