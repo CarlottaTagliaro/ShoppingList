0c403ffa7254e7f960c6d5455fb5440fc6bd98b5
caramellaio <albertobombardelli123@gmail.com>
2019-01-10 13:52:38 +0100

    fix query

--- hard_query.sql
+++ hard_query.sql
@@ -10,7 +10,7 @@ insert into Notifiche (ID_list, ID_prodotto, Giorni_mancanti, Quantita_mancanti,
 select final.ID_lista, final.ID_prodotto, final.giorni_manc, final.quant_manc, now() as ora, false as Mail from
 
 	(select ID_lista, ID_prodotto, 
-		minute(giorni_media - timediff(NOW(), ultimo)) as giorni_manc,
+		giorni_media - minute(NOW()) - minute(ultimo) as giorni_manc,
 		(quant_media + quant_media - 
 			(select Quantita from Liste_Prodotti_Acquistati where ID_lista = medie.ID_lista AND ID_prodotto = medie.ID_prodotto AND Data_acquisto = medie.ultimo)
 			) as quant_manc,
